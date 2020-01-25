b20188a23a23363f513c8983eaf637131ad5f4c8
anand-mcs <59893145+anand-mcs@users.noreply.github.com>
2020-01-25 11:02:30 +0530

    final baseline for download

---
+++ Implementor/IPaymentSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDesignPattern
+{
+    //Implementor Interface
+    public interface IPaymentSystem
+    {
+        void ProcessPayment(string PaymentSystem);
+    }
+}
